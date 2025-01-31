699c2f582535b7ce1c085ba85c6edcaf22a58838
Isaac Scarpelli <140021795+IsaacScarpelli@users.noreply.github.com>
2025-01-31 09:03:56 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import List from './components/List';
+function App() {
+
+const name = 'mateus'
+
+  return (
+    <div className="App">
+      <List/>
+    </div>
+  );
+}
+
+export default App;
